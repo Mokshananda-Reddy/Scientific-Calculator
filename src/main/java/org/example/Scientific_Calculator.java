cbcd5988d711dc5963b46be186aa6c28d0ee5f68
Mokshananda <Mokshananda.Reddy@iiitb.ac.in>
2023-03-14 19:06:23 +0530

    Updated the calculator code and tests

--- src/main/java/org/example/Scientific_Calculator.java
+++ src/main/java/org/example/Scientific_Calculator.java
@@ -1,4 +1,4 @@
-package org.Calc;
+package org.example;
 
 import java.util.Scanner;
 
@@ -10,84 +10,59 @@ public class Scientific_Calculator {
         Scanner myObj = new Scanner(System.in);
         System.out.println("The user has started the Scientific Calculator");
 
-        System.out.println("Enter the key number for the operation."+'\n'+"  The following is index 1: Square Root, 2: Factorial, 3: Natural Log, 4: Power, 5: Exit the calculator");
-        int key = myObj.nextInt();
-
-        System.out.println("Enter number of inputs");
-        int n_inp = myObj.nextInt();
-
-        double x = 0;
-        double y = 0;
-
-        if (n_inp == 1)
-        {
-            System.out.println("Enter the number");
-            x = myObj.nextDouble();
-        }
-
-        else
-        {
-            System.out.println("Enter the first number");
-            x = myObj.nextDouble();
-            System.out.println("Enter the second number");
-            y = myObj.nextDouble();
-        }
-
         double result = 0;
-        while(key != 5)
+        boolean exit = false;
+        while(!exit)
         {
+
+            System.out.println("Enter the key number for the operation."+'\n'+"  The following is index 1: Square Root, 2: Factorial, 3: Natural Log, 4: Power, 5: Exit the calculator");
+            int key = myObj.nextInt();
+
+            double x = 0;
+            double y = 0;
+
             if (key == 1)
             {
+                System.out.println("Enter the number");
+                x = myObj.nextDouble();
                 result = calculator.sq_root(x);
                 System.out.println("The required result is :" + result);
             }
             else if (key == 2)
             {
+
+                System.out.println("Enter the number");
+                x = myObj.nextDouble();
                 result = calculator.fact(x);
                 System.out.println("The required result is :" + result);
             }
             else if (key == 3)
             {
+                System.out.println("Enter the number");
+                x = myObj.nextDouble();
                 result = calculator.nlog(x);
                 System.out.println("The required result is :" + result);
             }
             else if (key == 4)
             {
-                result = calculator.power(x,y);
-                System.out.println("The required result is :" + result);
-            }
-
-            System.out.println("Enter the key number for the operation");
-            key = myObj.nextInt();
-
-            if(key == 5)
-            {
-                System.out.println("The user has closed the Scientific Calculator");
-                break;
-            }
-
-            System.out.println("Enter number of inputs");
-            n_inp = myObj.nextInt();
-
-            if (n_inp == 1)
-            {
-                System.out.println("Enter the number");
-                x = myObj.nextDouble();
-            }
-
-            else
-            {
                 System.out.println("Enter the first number");
                 x = myObj.nextDouble();
                 System.out.println("Enter the second number");
                 y = myObj.nextDouble();
+                result = calculator.power(x,y);
+                System.out.println("The required result is :" + result);
+            }
+
+            else if(key == 5)
+            {
+                System.out.println("The user has closed the Scientific Calculator");
+                exit = true;
+                break;
             }
 
         }
 
     }
-
-    // sq root, fact, natural log, x^y
 
     public double sq_root(double num) {
 
